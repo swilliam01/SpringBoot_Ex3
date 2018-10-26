9d1c604af7265d5ae38b57feb0f15120f611c4d8
swilliam01 <sehrishwilliam@gmail.com>
2018-10-25 22:21:34 -0400

    changed it

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -29,7 +26,7 @@ public class HomeController {
         return "listform";
     }
     @PostMapping("/process")
-    public String processList (@Valid TodoList listToday, BindingResult result){
+    public String processList (@Valid @ModelAttribute("listToday") TodoList listToday, BindingResult result){
         if(result.hasErrors()){
             return "listform";
         }
